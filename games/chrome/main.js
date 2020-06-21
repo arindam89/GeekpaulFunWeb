2e02757efe2fac0347857ad9167516539214efb3
Arindam Paul <arindampaul1989@gmail.com>
2020-06-21 20:04:08 +0530

    Added the JS Game Dinosaur

---
+++ games/chrome/main.js
@@ -0,0 +1,33 @@
+const game = document.getElementById("main");
+const hero = document.getElementById("hero");
+const tree = document.getElementById("tree");
+
+function handleClick() {
+  if (hero.classList.contains("jumping")) {
+    hero.classList.remove("jumping");
+  }
+  hero.classList.add("jumping");
+  setTimeout(() => {
+    hero.classList.remove("jumping");
+  }, 600);
+}
+
+game.addEventListener("click", handleClick);
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    handleClick();
+  }
+});
+
+setInterval(() => {
+  const heroY = parseInt(window.getComputedStyle(hero).top);
+  const treeX = parseInt(window.getComputedStyle(tree).left);
+
+  // Game Over Condition
+  if (treeX < 30 && treeX > 10 && heroY > 160) {
+    alert("Game over");
+    tree.remove();
+    tree.style.transition = "none";
+  }
+}, 10);
