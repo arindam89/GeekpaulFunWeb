e8bf083bbd07d9df16e393700ca165a13966476d
Arindam Paul <arindampaul1989@gmail.com>
2020-06-23 00:04:51 +0530

    Touch friendly keys are added as well

--- games/subway_surfer/main.js
+++ games/subway_surfer/main.js
@@ -10,22 +10,50 @@ obstacle.addEventListener("animationiteration", () => {
   obstacle.style.left = left + "px";
 });
 
+const moveLeft = () => {
+  const old_left = parseInt(window.getComputedStyle(hero).left);
+  if (old_left > 0) {
+    const new_left = old_left - 100;
+    hero.style.left = new_left + "px";
+  }
+};
+
+const moveRight = () => {
+  const old_left = parseInt(window.getComputedStyle(hero).left);
+  if (old_left < 200) {
+    const new_left = old_left + 100;
+    hero.style.left = new_left + "px";
+  }
+};
+
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowLeft") {
     // Move left;
-    const old_left = parseInt(window.getComputedStyle(hero).left);
-    if (old_left > 0) {
-      const new_left = old_left - 100;
-      hero.style.left = new_left + "px";
-    }
+    moveLeft();
   }
-
   if (event.key === "ArrowRight") {
     // Move right;
-    const old_left = parseInt(window.getComputedStyle(hero).left);
-    if (old_left < 200) {
-      const new_left = old_left + 100;
-      hero.style.left = new_left + "px";
-    }
+    moveRight();
   }
 });
+
+// Making things touch friendly
+const left_area = document.getElementById("left");
+const right_area = document.getElementById("right");
+
+left_area.addEventListener("touchstart", moveLeft);
+right_area.addEventListener("touchstart", moveRight);
+
+// Game condition checker, runs every 10ms
+
+setInterval(() => {
+  const obstacle_left = parseInt(window.getComputedStyle(obstacle).left);
+  const obstacle_top = parseInt(window.getComputedStyle(obstacle).top);
+  const hero_left = parseInt(window.getComputedStyle(hero).left);
+
+  if (obstacle_left == hero_left && obstacle_top > 400 && obstacle_top < 600) {
+    obstacle.style.transition = "none";
+    obstacle.remove();
+    alert("Collision");
+  }
+}, 10);
